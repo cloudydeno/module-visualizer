fa6a456bffc2bdc431071ee1857f66df3aa733ec
Daniel Lamando <dan@danopia.net>
2021-04-24 22:58:54 +0200

    registry-key: Reorganize page

--- feat/registry-key/api.ts
+++ feat/registry-key/api.ts
@@ -7,12 +7,21 @@ export async function handleRequest(req: http.ServerRequest) {
 
   const moduleKey = Object
     .entries(ModuleColors)
+    .filter(x => x[0].includes('.'))
+    .map(([registry, color]) =>
+      `<div style="background-color: ${entities.encode(color)}">${entities.encode(registry)}</div>`)
+    .join(`\n`);
+
+  const extraKey = Object
+    .entries(ModuleColors)
+    .filter(x => !x[0].includes('.'))
     .map(([registry, color]) =>
       `<div style="background-color: ${entities.encode(color)}">${entities.encode(registry)}</div>`)
     .join(`\n`);
 
   await serveTemplatedHtml(req, 'feat/registry-key/public.html', {
     module_key: moduleKey.replace(/^/gm, '  '),
+    extra_key: extraKey.replace(/^/gm, '  '),
   });
 
   return true;
