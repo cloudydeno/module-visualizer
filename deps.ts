4037d3da1ee8b8661a1ee4dcb4463b4a89231680
Daniel Lamando <dan@danopia.net>
2021-05-04 21:00:53 +0200

    Add some things back to deps.ts

--- deps.ts
+++ deps.ts
@@ -3,3 +3,8 @@ export * as file_server from "https://deno.land/std@0.95.0/http/file_server.ts";
 export { readerFromIterable } from "https://deno.land/std@0.95.0/io/streams.ts";
 
 export * as entities from "https://deno.land/x/html_entities@v1.0/lib/xml-entities.js";
+
+// these are also imported in other places
+export { SubProcess } from "https://crux.land/454pqj#sub-process@v2";
+export type { SubprocessErrorData } from "https://crux.land/454pqj#sub-process@v2";
+export { filesize } from "https://crux.land/6wZ5Sz#filesize@v1";
